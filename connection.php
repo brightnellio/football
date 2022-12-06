49d11b7a78d82ab4e237e2d5b8ef2b2d9394aeb9
brightnellio <115991411+brightnellio@users.noreply.github.com>
2022-12-06 01:03:29 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("localhost","root","","academy");
+    $database= new mysqli("localhost","root","","heroku_ee2ec3cda07786f");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }

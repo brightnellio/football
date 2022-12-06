e26e7c752eec9414e1e7091ad9977d5a6fd5fa37
brightnellio <115991411+brightnellio@users.noreply.github.com>
2022-12-06 08:00:49 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,15 @@
 <?php
 
-    $database= new mysqli("localhost","root","","heroku_ee2ec3cda07786f");
-    if ($database->connect_error){
-        die("Connection failed:  ".$database->connect_error);
-    }
+   <?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("mysql://b606718d849413:5f793c83@us-cdbr-east-06.cleardb.net/heroku_ee2ec3cda07786f?reconnect=true"));
+$cleardb_server = $cleardb_url["us-cdbr-east-06.cleardb.net"];
+$cleardb_username = $cleardb_url["b606718d849413"];
+$cleardb_password = $cleardb_url["5f793c83"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 ?>
